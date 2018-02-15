3a80b40585b04bfa2f46a8285489480787cd6837
Chris.V <cvasqu09@gmail.com>
2018-02-15 17:11:33 -0600

    Converted seed data to json. Added clearing of events database

--- seed-db.rb
+++ seed-db.rb
@@ -1,6 +1,7 @@
 require 'roo'
 require 'json'
 require 'byebug'
+require 'mongo'
 require 'rest-client'
 
 class MongoSeeder
@@ -11,6 +12,7 @@ class MongoSeeder
 	end
 
 	def seed_events
+		clear_events_db()
 		count = 1
 		keys = []
 		@xlsx.each do |row|
@@ -38,6 +40,7 @@ class MongoSeeder
 				values.each { |e| e.strip! }
 				request[json_key] = values
 			else 
+				next if row[index] == nil
 				json_key = key
 				value = row[index]
 				request[json_key] = value
@@ -53,6 +56,12 @@ class MongoSeeder
 		rescue => e
 			raise "Error occurred: #{e.response}"
 		end
+	end
+
+	def clear_events_db
+		client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'LiveDB')
+		client[:events].delete_many({})
+		client.close
 	end
 end
 
