1afacbd27ee52b0569c181616441b562b27fb1b7
Chris.V <cvasqu09@gmail.com>
2018-02-18 12:39:07 -0600

    Added user to seeder

--- seed-db.rb
+++ seed-db.rb
@@ -7,32 +7,41 @@ require 'json'
 
 class MongoSeeder
 
-	def initialize(path_to_events_json)
-		file = File.read(path_to_events_json)
-		@events = JSON.parse(file) 
+	def initialize(path_to_events_json, path_to_users_json)
+		events_json_file = File.read(path_to_events_json)
+		users_json_file = File.read(path_to_users_json)
+		@events = JSON.parse(events_json_file)
+		@users = JSON.parse(users_json_file)
 	end
 
 	def seed_events
-		clear_events_db
+		clear_db('events')
 		@events.each do |event|
-			post_event(event.to_json)
+			post('http://localhost:3000/api/events', event.to_json)
+		end
+	end
+
+	def seed_users
+		clear_db('users')
+		@users.each do |user|
+			post('http://localhost:3000/api/users', user.to_json)
 		end
 	end
 
 	private 
 
-	def post_event(request)
+	def post(endpoint, request)
 		puts "posting: #{request}"
 		begin
-			response = RestClient.post 'http://localhost:3000/api/events', request, {content_type: :json, accept: :json}
+			response = RestClient.post endpoint, request, {content_type: :json, accept: :json}
 		rescue => e
 			raise "Error occurred: #{e.response}"
 		end
 	end
 
-	def clear_events_db
+	def clear_db(db_name)
 		client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'LiveDB')
-		client[:events].delete_many({})
+		client[db_name.to_sym].delete_many({})
 		client.close
 	end
 end
